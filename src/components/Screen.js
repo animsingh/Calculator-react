3b978b5e395181aeed0880b61f3a9fc1a2ed5978
Animesh <animessingh15@gmail.com>
2021-12-03 22:52:34 +0530

    mysecondcommit

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,11 +1,11 @@
-import { Textfit } from "react-textfit";
+
 import "./Screen.css";
 
 const Screen = ({ value }) => {
   return (
-    <Textfit className="screen" mode="single" max={70}>
+    <p className="screen" mode="single" max={70}>
       {value}
-    </Textfit>
+    </p>
   );
 };
 
